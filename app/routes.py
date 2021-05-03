6cdaf27fa1e0f6b693806a7b7775cba059d9ad82
geer26 <gergo.kurucz@gmail.com>
2021-05-03 09:38:27 +0200

    v_0.2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, jsonify
 #from flask_login import current_user, login_user, logout_user, login_required
-from app import app, limiter
+from app import app #limiter
 #from app.models import User
 
 
